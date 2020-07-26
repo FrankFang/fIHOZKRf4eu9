7e80e71e9efbc10a84fe5100387f116113e89d88
frankfang <frankfang1990@gmail.com>
2020-07-27 06:25:27 +0800

    整体布局 css

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -25,7 +25,10 @@ export default {
   background: pink;
   display: flex;
   padding: 16px;
-  position: relative;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
   z-index: 10;
   justify-content: center;
   align-items: center;
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <Topnav />
+  <div class="layout">
+    <Topnav class="nav" />
     <div class="content">
       <aside v-if="menuVisible">
         <h2>组件列表</h2>
@@ -37,10 +37,42 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+.layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  > .nav {
+    flex-shrink: 0;
+  }
+  > .content {
+    flex-grow: 1;
+    padding-top: 60px;
+    padding-left: 156px;
+    @media (max-width: 500px) {
+      padding-left: 0; 
+    }
+  }
+}
+.content {
+  display: flex;
+  > aside {
+    flex-shrink: 0;
+  }
+  > main {
+    flex-grow: 1;
+    padding: 16px;
+    background: lightgreen;
+  }
+}
 aside {
   background: lightblue;
   width: 150px;
   padding: 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding-top: 70px;
+  height: 100%;
   > h2 {
     margin-bottom: 4px;
   }
@@ -49,11 +81,8 @@ aside {
       padding: 4px 0;
     }
   }
-  @media (max-width: 500px) {
-    position: fixed;
-    top: 0;
-    left: 0;
-    padding-top: 70px;
-  }
+}
+main {
+  overflow: auto;
 }
 </style>
