c249193231dbbd037732a6c1c0df65d267e65201
frankfang <frankfang1990@gmail.com>
2020-07-27 05:51:08 +0800

    添加 toggleAside

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -41,7 +41,16 @@ export default {
       margin: 0 1em;
     }
   }
-  > .toggleMenu {}
+  > .toggleAside {
+    display: inline-block;
+    width: 24px;
+    height: 24px;
+    background: red;
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+  }
   @media (max-width:500px) {
     > .menu{display: none;} 
     > .logo{margin: 0 auto;}
