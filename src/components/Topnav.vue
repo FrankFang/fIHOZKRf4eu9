01b7ce07ad0d5935f6059fccf6b7dfffbdc12b5e
frankfang <frankfang1990@gmail.com>
2020-07-27 05:48:59 +0800

    让 topnav 是响应式的

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -5,6 +5,7 @@
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
+    <span class="toggleAside"></span>
   </div>
 </template>
 <script lang="ts">
@@ -26,6 +27,8 @@ export default {
   padding: 16px;
   position: relative;
   z-index: 10;
+  justify-content: center;
+  align-items: center;
   > .logo {
     max-width: 6em;
     margin-right: auto;
@@ -37,6 +40,11 @@ export default {
     > li {
       margin: 0 1em;
     }
+  }
+  > .toggleMenu {}
+  @media (max-width:500px) {
+    > .menu{display: none;} 
+    > .logo{margin: 0 auto;}
   }
 }
 </style>
