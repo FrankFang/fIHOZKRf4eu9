083d5490652e756d5cc867a29b1a1720b3876992
frankfang <frankfang1990@gmail.com>
2020-07-27 04:32:09 +0800

    topnav 样式更新

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './index.css'
+import './index.scss'
 import {createWebHashHistory, createRouter} from 'vue-router'
 import Home from './views/Home.vue'
 import Doc from './views/Doc.vue'
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <div class="topnav">
-      <div class="logo"></div>
-      <div class="menu"></div>
+      <div class="logo">LOGO</div>
+      <ul class="menu">
+        <li>菜单1</li>
+        <li>菜单2</li>
+      </ul>
     </div>
     <div class="banner">
       <h1>轱辘UI</h1>
@@ -15,6 +18,21 @@
   </div>
 </template>
 <style lang="scss" scoped>
-  .topnar{border: 1px solid red;}
-  
+.topnav {
+  background: pink;
+  display: flex;
+  padding: 16px;
+  > .logo {
+    max-width: 6em;
+    margin-right: auto;
+  }
+  > .menu {
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+    > li {
+      margin: 0 1em;
+    }
+  }
+}
 </style>
